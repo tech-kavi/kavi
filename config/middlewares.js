8dae94f507659a52d10a1bb11efbffd103a414e9
tech@kavi <tech@kaviresearch.in>
2024-04-11 13:45:24 +0530

    localhost also added to strapi cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -12,7 +12,7 @@ module.exports = [
   {
     name:'strapi::cors',
     config:{ 
-    origin: ['https://3dbfa084-3e4a-429b-bdab-3df3b23a9315.weweb-preview.io'],
+    origin: ['https://3dbfa084-3e4a-429b-bdab-3df3b23a9315.weweb-preview.io','http://localhost:1337'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
     headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     keepHeaderOnError: true,
