d14e482c8d3a8513e4bdd33da9d7e78d703e41af
tech@kavi <tech@kaviresearch.in>
2024-04-11 13:59:49 +0530

    removed cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,14 +9,5 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name:'strapi::cors',
-    config:{ 
-    origin: ['https://3dbfa084-3e4a-429b-bdab-3df3b23a9315.weweb-preview.io','http://localhost:1337'],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-    headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-    keepHeaderOnError: true,
-    },
-  },
-  
+
 ];
