9d21de0970fae0194d1381adf8a5f1158bcfd1b6
tech@kavi <tech@kaviresearch.in>
2024-04-11 16:10:21 +0530

    another try to add CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,17 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  // 'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ["https://3dbfa084-3e4a-429b-bdab-3df3b23a9315.weweb-preview.io", "https://dazzling-butterfly-f3a6f1abbb.strapiapp.com"], // Add your project's domain and IP addresses
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      headers: ["Authorization", "Content-Type"],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
