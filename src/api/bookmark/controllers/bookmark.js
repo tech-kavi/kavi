980ee8015303a7df53d942130c3fc524fbccf248
tech@kavi <tech@kaviresearch.in>
2024-05-29 19:02:30 +0530

    bookmark controller customized

--- src/api/bookmark/controllers/bookmark.js
+++ src/api/bookmark/controllers/bookmark.js
@@ -5,7 +5,7 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
-
+// module.exports = createCoreController('api::bookmark.bookmark');
 module.exports = createCoreController('api::bookmark.bookmark',{
     async find(ctx){
         const {user}=ctx.state;
@@ -13,7 +13,23 @@ module.exports = createCoreController('api::bookmark.bookmark',{
             return ctx.unauthorized('you must be logged in to access bookmarks');
         }
         const entity = await strapi.db.query('api::bookmark.bookmark').findMany({
-            where:{bookmarked_by:user.id}
+            where:{bookmarked_by:user.id},
+            populate:{
+                article:{
+                    populate:{
+                        industry:true,
+                        primary_company:{
+                            populate:{
+                                logo:true
+                            }
+                        },
+                        
+                    },
+
+            },
+        },
+        limit:-1,
+      
         });
         const sanitizedEntity = await this.sanitizeOutput(entity,ctx);
 
