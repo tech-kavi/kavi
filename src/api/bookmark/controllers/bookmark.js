eeef100d56cf90ad487bcf644f3eda0435f6bc57
tech@kavi <tech@kaviresearch.in>
2024-05-29 19:29:26 +0530

    change in bookmark controller

--- src/api/bookmark/controllers/bookmark.js
+++ src/api/bookmark/controllers/bookmark.js
@@ -6,33 +6,67 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 // module.exports = createCoreController('api::bookmark.bookmark');
-module.exports = createCoreController('api::bookmark.bookmark',{
-    async find(ctx){
-        const {user}=ctx.state;
-        if(!user){
-            return ctx.unauthorized('you must be logged in to access bookmarks');
-        }
-        const entity = await strapi.db.query('api::bookmark.bookmark').findMany({
-            where:{bookmarked_by:user.id},
-            populate:{
-                article:{
-                    populate:{
-                        industry:true,
-                        primary_company:{
-                            populate:{
-                                logo:true
-                            }
-                        },
+// module.exports = createCoreController('api::bookmark.bookmark',{
+//     async find(ctx){
+//         const {user}=ctx.state;
+//         if(!user){
+//             return ctx.unauthorized('you must be logged in to access bookmarks');
+//         }
+//         const entity = await strapi.db.query('api::bookmark.bookmark').findMany({
+//             where:{bookmarked_by:user.id},
+//             populate:{
+//                 article:{
+//                     populate:{
+//                         industry:true,
+//                         primary_company:{
+//                             populate:{
+//                                 logo:true
+//                             }
+//                         },
                         
-                    },
+//                     },
 
-            },
-        },
-        limit:-1,
+//             },
+//         },
+//         limit:-1,
       
-        });
-        const sanitizedEntity = await this.sanitizeOutput(entity,ctx);
+//         });
+//         const sanitizedEntity = await this.sanitizeOutput(entity,ctx);
 
+//         return this.transformResponse(sanitizedEntity);
+//     }
+// });
+
+module.exports = createCoreController('api::bookmark.bookmark', {
+    async find(ctx) {
+      const { user } = ctx.state;
+      if (!user) {
+        return ctx.unauthorized('You must be logged in to access bookmarks');
+      }
+      try {
+        const entity = await strapi.db.query('api::bookmark.bookmark').findMany({
+          where: { bookmarked_by: user.id },
+          populate: {
+            article: {
+              populate: {
+                industry: true,
+                primary_company: {
+                  populate: {
+                    logo: true
+                  }
+                }
+              }
+            }
+          },
+          limit: -1,
+        });
+  
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
         return this.transformResponse(sanitizedEntity);
+  
+      } catch (error) {
+        console.error('Error in find method:', error);
+        return ctx.internalServerError('Internal server error');
+      }
     }
-});
+  });
