7f4f05231098d42f688f70d028188d88e8f40ae2
nishant <tech@kaviresearch.in>
2024-09-05 13:11:05 +0530

    commented non-useful data from /article findOne api

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -210,7 +210,7 @@ module.exports = createCoreController('api::article.article',{
             populate: {
                 brief: true,
                 industry: {
-                    fields: ['name', 'articles'],
+                    fields: ['name'],
                     filters:{
                         publishedAt:{
                             $notNull:true,
@@ -271,161 +271,161 @@ module.exports = createCoreController('api::article.article',{
        const read_time=this.countWordsInFields(article);
 
     
-        const primaryCompanies = article.data.attributes.primary_companies.data.map(company => company.id);
+    //     const primaryCompanies = article.data.attributes.primary_companies.data.map(company => company.id);
     
-        // Fetching articles & secondary articles for primary companies 
-        const primaryArticlesPromises = primaryCompanies.map(async (company) => {
-            const companyWithArticles = await strapi.entityService.findOne(
-                'api::company.company', company,
-                {
-                    populate: {
-                        articles: {
-                            populate: {
-                                primary_companies: {
-                                    fields: ['name'],
-                                    populate: {
-                                        logo: true,
-                                    },
-                                    filters:{
-                                        publishedAt:{
-                                            $notNull:true,
-                                        }
-                                    }
-                                },
-                                industry: {
-                                    filters:{
-                                        publishedAt:{
-                                            $notNull:true,
-                                        }
-                                    }
-                                },
-                                brief:true,
-                                table_with_content: {
-                                populate: {
-                                    ques: true,
-                                }
-                                }
-                            },
-                            filters: {
-                                publishedAt: {
-                                    $notNull: true,
-                                },
-                            },
+    //     // Fetching articles & secondary articles for primary companies 
+    //     const primaryArticlesPromises = primaryCompanies.map(async (company) => {
+    //         const companyWithArticles = await strapi.entityService.findOne(
+    //             'api::company.company', company,
+    //             {
+    //                 populate: {
+    //                     articles: {
+    //                         populate: {
+    //                             primary_companies: {
+    //                                 fields: ['name'],
+    //                                 populate: {
+    //                                     logo: true,
+    //                                 },
+    //                                 filters:{
+    //                                     publishedAt:{
+    //                                         $notNull:true,
+    //                                     }
+    //                                 }
+    //                             },
+    //                             industry: {
+    //                                 filters:{
+    //                                     publishedAt:{
+    //                                         $notNull:true,
+    //                                     }
+    //                                 }
+    //                             },
+    //                             brief:true,
+    //                             table_with_content: {
+    //                             populate: {
+    //                                 ques: true,
+    //                             }
+    //                             }
+    //                         },
+    //                         filters: {
+    //                             publishedAt: {
+    //                                 $notNull: true,
+    //                             },
+    //                         },
     
-                        },
-                        secondary_articles: {
-                            populate: {
-                                primary_companies: {
-                                    fields: ['name'],
-                                    populate: {
-                                        logo: true,
-                                    },
-                                    filters:{
-                                        publishedAt:{
-                                            $notNull:true,
-                                        }
-                                    }
-                                },
-                                industry: {
-                                    filters:{
-                                        publishedAt:{
-                                            $notNull:true,
-                                        }
-                                    }
-                                },
-                                brief:true,
-                                table_with_content: {
-                                populate: {
-                                    ques: true,
-                                }
-                            }
-                            },
-                            filters: {
-                                publishedAt: {
-                                    $notNull: true,
-                                }
-                            }
-                        },
-                    }
-                }
-            );
-            return {
-                articles: companyWithArticles.articles,
-                secondary_articles: companyWithArticles.secondary_articles,
-            };
-        });
+    //                     },
+    //                     secondary_articles: {
+    //                         populate: {
+    //                             primary_companies: {
+    //                                 fields: ['name'],
+    //                                 populate: {
+    //                                     logo: true,
+    //                                 },
+    //                                 filters:{
+    //                                     publishedAt:{
+    //                                         $notNull:true,
+    //                                     }
+    //                                 }
+    //                             },
+    //                             industry: {
+    //                                 filters:{
+    //                                     publishedAt:{
+    //                                         $notNull:true,
+    //                                     }
+    //                                 }
+    //                             },
+    //                             brief:true,
+    //                             table_with_content: {
+    //                             populate: {
+    //                                 ques: true,
+    //                             }
+    //                         }
+    //                         },
+    //                         filters: {
+    //                             publishedAt: {
+    //                                 $notNull: true,
+    //                             }
+    //                         }
+    //                     },
+    //                 }
+    //             }
+    //         );
+    //         return {
+    //             articles: companyWithArticles.articles,
+    //             secondary_articles: companyWithArticles.secondary_articles,
+    //         };
+    //     });
     
-        const primaryArticlesResults = await Promise.all(primaryArticlesPromises);
+    //     const primaryArticlesResults = await Promise.all(primaryArticlesPromises);
     
-        let relatedArticles = primaryArticlesResults.flatMap(result => [...result.articles, ...result.secondary_articles]);
-        relatedArticles = relatedArticles.filter(x => x.id != currentArticleId);
+    //     let relatedArticles = primaryArticlesResults.flatMap(result => [...result.articles, ...result.secondary_articles]);
+    //     relatedArticles = relatedArticles.filter(x => x.id != currentArticleId);
 
-        // console.log(relatedArticles);
+    //     // console.log(relatedArticles);
     
-        // Fetch sub-industry and industry articles only if sub-industry is not "Miscellaneous"
-        const subIndustry = article.data.attributes.sub_industries.data[0]?.attributes.name;
-        if (subIndustry && subIndustry !== "Miscellaneous" && subIndustry !== "Marketplace" && subIndustry !== "Diversified") {
-            // If we don't have enough articles, fetch sub-industry articles
-            if (relatedArticles.length < 3) {
-                const subIndustryArticles = await strapi.entityService.findMany('api::article.article', {
-                    filters: {
-                        sub_industries: { name: subIndustry },
-                        publishedAt: { $notNull: true },
-                        id: { $ne: currentArticleId }
-                    },
-                    populate: {
-                        primary_companies: {
-                            fields: ['name'],
-                            populate: { logo: true },
-                            filters:{
-                                publishedAt:{
-                                    $notNull:true,
-                                }
-                            }
-                        },
-                        industry: {
-                            filters:{
-                                publishedAt:{
-                                    $notNull:true,
-                                }
-                            }
-                        },
-                        brief:true,
-                        table_with_content: {
-                                populate: {
-                                    ques: true,
-                                }
-                        }
-                    }
-                });
-                relatedArticles.push(...subIndustryArticles);
-            }
+    //     // Fetch sub-industry and industry articles only if sub-industry is not "Miscellaneous"
+    //     const subIndustry = article.data.attributes.sub_industries.data[0]?.attributes.name;
+    //     if (subIndustry && subIndustry !== "Miscellaneous" && subIndustry !== "Marketplace" && subIndustry !== "Diversified") {
+    //         // If we don't have enough articles, fetch sub-industry articles
+    //         if (relatedArticles.length < 3) {
+    //             const subIndustryArticles = await strapi.entityService.findMany('api::article.article', {
+    //                 filters: {
+    //                     sub_industries: { name: subIndustry },
+    //                     publishedAt: { $notNull: true },
+    //                     id: { $ne: currentArticleId }
+    //                 },
+    //                 populate: {
+    //                     primary_companies: {
+    //                         fields: ['name'],
+    //                         populate: { logo: true },
+    //                         filters:{
+    //                             publishedAt:{
+    //                                 $notNull:true,
+    //                             }
+    //                         }
+    //                     },
+    //                     industry: {
+    //                         filters:{
+    //                             publishedAt:{
+    //                                 $notNull:true,
+    //                             }
+    //                         }
+    //                     },
+    //                     brief:true,
+    //                     table_with_content: {
+    //                             populate: {
+    //                                 ques: true,
+    //                             }
+    //                     }
+    //                 }
+    //             });
+    //             relatedArticles.push(...subIndustryArticles);
+    //         }
     
          
-        }
+    //     }
 
        
 
-        // //remove duplicate articles by ID using a Set
-        relatedArticles = Array.from(new Set(relatedArticles.map(article => article.id)))
-        .map(id => relatedArticles.find(article => article.id === id));
+    //     // //remove duplicate articles by ID using a Set
+    //     relatedArticles = Array.from(new Set(relatedArticles.map(article => article.id)))
+    //     .map(id => relatedArticles.find(article => article.id === id));
 
-        // Sort related articles by latest publishedAt date
-    relatedArticles = relatedArticles.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+    //     // Sort related articles by latest publishedAt date
+    // relatedArticles = relatedArticles.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
     
-        // Ensure only 3 related articles are returned
-        relatedArticles = relatedArticles.slice(0, 3);
+    //     // Ensure only 3 related articles are returned
+    //     relatedArticles = relatedArticles.slice(0, 3);
 
-        //calculate read time for each article
-    const RelatedArticlesWithReadTime = relatedArticles.map(article =>{
+    //     //calculate read time for each article
+    // const RelatedArticlesWithReadTime = relatedArticles.map(article =>{
       
-        const readTime = this.countWordsInFieldsOfRelatedArticles(article);
+    //     const readTime = this.countWordsInFieldsOfRelatedArticles(article);
         
-        return{
-          ...article,
-          read_time:readTime,
-        };
-      })
+    //     return{
+    //       ...article,
+    //       read_time:readTime,
+    //     };
+    //   })
     
         // Fetching bookmarked, liked, and disliked articles for the user
         const [bookmarkedArticles, likedArticles, dislikedArticles] = await Promise.all([
@@ -448,12 +448,12 @@ module.exports = createCoreController('api::article.article',{
         const DisLikeArticleIds = dislikedArticles.map(dislikedArticle => dislikedArticle.article.id);
     
         // Adding bookmark status to related articles
-        const articleWithBookmarkStatus = RelatedArticlesWithReadTime.map(article => ({
-            ...article,
-            isBookmarked: BookmarkArticleIds.includes(article.id),
-        }));
+        // const articleWithBookmarkStatus = RelatedArticlesWithReadTime.map(article => ({
+        //     ...article,
+        //     isBookmarked: BookmarkArticleIds.includes(article.id),
+        // }));
     
-        article.data.attributes.relatedArticles = articleWithBookmarkStatus;
+        // article.data.attributes.relatedArticles = articleWithBookmarkStatus;
     
         // Adding like/dislike/bookmark status to the current article
         article.data.attributes.isLiked = LikeArticleIds.includes(article.data.id);
